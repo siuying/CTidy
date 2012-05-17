f4c73ee4e60cc3c2e6a0c61d66937e1927bccb14
Francis Chong <francis@ignition.hk>
2012-05-17 16:57:44 +0800

    update the rubymotion library

--- motion/tidy.rb
+++ motion/tidy.rb
@@ -4,7 +4,7 @@ module Tidy
   def XHTML(html, encoding="UTF8")
     error_ptr = Pointer.new(:id)
     diagnostics_ptr = Pointer.new(:object)
-    xhtml = CTidy.tidy.tidyString(html, inputFormat:0, outputFormat:2, encoding:encoding, diagnostics:diagnostics_ptr, error:error_ptr)
+    xhtml = CTidy.tidy.tidyHTMLString(html, encoding:encoding, error:error_ptr)
     raise error_ptr[0].description if error_ptr[0]
     xhtml
   end
