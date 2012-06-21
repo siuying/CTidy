34de87d0b7ebb9e5d953f95f6ec1912b3b0fa91d
Francis Chong <francis@ignition.hk>
2012-06-21 18:10:08 +0800

    use back pod

--- motion/lib/motion-tidy.rb
+++ motion/lib/motion-tidy.rb
@@ -9,5 +9,16 @@ Motion::Project::App.setup do |app|
     app.files.unshift(file)
   end
 
-  app.vendor_project(File.expand_path(File.join(File.dirname(__FILE__), '../../CTidy.xcodeproj')), :static, :products => ["libCTidy.a"])
+  app.pods ||= Motion::Project::CocoaPods.new(app)
+  app.pods.dependency do |s|
+    s.name     = 'CTidy'
+    s.version  = '0.2.1.pre'
+    s.license  = 'Simplified BSD License'
+    s.summary  = "libtidy Objective-C wrapper"
+    s.homepage = 'https://github.com/siuying/CTidy'
+    s.author   = { 'Francis Chong' => 'francis@ignition.hk' }
+    s.source   = { :git => 'git://github.com/siuying/CTidy.git', :commit => 'd13e7f13e07ec39c56b93a73b8bd3e335d86a0c8' }
+    s.source_files = 'CTidy/CTidy.{h,m}', 'libtidy/*.{c,h}'
+    s.clean_paths  = '*.xcodeproj'
+  end
 end
