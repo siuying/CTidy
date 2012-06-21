99179fd062f2c57319bea91148041ca9250abd9d
Francis Chong <francis@ignition.hk>
2012-06-21 18:17:37 +0800

    correct lib path

--- motion/lib/motion-tidy.rb
+++ motion/lib/motion-tidy.rb
@@ -18,7 +18,7 @@ Motion::Project::App.setup do |app|
     s.homepage = 'https://github.com/siuying/CTidy'
     s.author   = { 'Francis Chong' => 'francis@ignition.hk' }
     s.source   = { :git => 'git://github.com/siuying/CTidy.git', :commit => 'd13e7f13e07ec39c56b93a73b8bd3e335d86a0c8' }
-    s.source_files = 'CTidy/CTidy.{h,m}', 'libtidy/*.{c,h}'
+    s.source_files = 'CTidy/CTidy.{h,m}', 'libtidy/**/*.{c,h}'
     s.clean_paths  = '*.xcodeproj'
   end
 end
